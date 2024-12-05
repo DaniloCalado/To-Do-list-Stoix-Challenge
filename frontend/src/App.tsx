7fc00d953b9dc9988a571597099fa56ea5a8eb21
DaniloCalado <dcalado.fisio@yahoo.com.br>
2024-12-05 01:20:40 -0300

    interface task e component tasktable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TaskInput from "./components/TaskInput";
+import TaskTable from "./components/TasksTable";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Task } from "./types/task";
+import { fetchTasks } from "./services/api";
+
 
 const App: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasks = await fetchTasks();
+      setTasks(tasks);
+    };
+
+    getTasks();
+  }, []);
+
+
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-2xl font-bold text-center mb-4">Minha Lista de Tarefas</h1>
       <TaskInput />
       <ToastContainer position="top-center" autoClose={3000} />
+      <TaskTable tasks={tasks} onEdit={function (id: number): void {
+        throw new Error("Function not implemented.");
+      } } onDelete={function (id: number): void {
+        throw new Error("Function not implemented.");
+      } } onToggleCompleted={function (id: number): void {
+        throw new Error("Function not implemented.");
+      } }  />
     </div>
   );
 };

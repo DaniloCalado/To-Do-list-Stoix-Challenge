f0e2ce0b7b396467573ca182a5736d0fe3170e55
DaniloCalado <dcalado.fisio@yahoo.com.br>
2024-12-10 17:15:39 -0300

    ajustando o cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,27 +2,33 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-// import csrf from 'csurf';
+import csrf from 'csurf';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      'https://to-do-list-stoix-challenge-tphk.vercel.app', 
+      'http://localhost:3000', 
+    ],
+    credentials: true,
+  })
+);
 
-app.use(cors({ origin: '*', credentials: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
-// app.use(csrf({ cookie: true }));
+app.use(csrf({ cookie: true }));
 
 import taskRoutes from './routes/taskRoutes';
 app.use('/api/tasks', taskRoutes);
 
-
-// app.get('/api/csrf-token', (req, res) => {
-//   res.json({ csrfToken: req.csrfToken() });
-// });
-
+app.get('/api/csrf-token', (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
 
 const PORT = parseInt(process.env.PORT || "10000", 10);
-app.listen(PORT, "0.0.0.0",  () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Servidor rodando na porta ${PORT}`));
